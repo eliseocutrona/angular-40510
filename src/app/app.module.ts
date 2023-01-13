b8753256d9df4ec5ce314aa4dfc5ce1b1e82eb3d
eliseocutrona <eliseocutrona@gmail.com>
2023-01-13 20:59:02 -0300

    primer desafio angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { StudentsComponent } from './components/students/students.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+   StudentsComponent,
+     ToolbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
